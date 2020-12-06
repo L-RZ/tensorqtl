a15cb38ffafb24885b20c4adedf19276ed962331
L-RZ <45250925+L-RZ@users.noreply.github.com>
2020-12-05 23:11:29 -0500

    Update install_cuda.sh

--- install/install_cuda.sh
+++ install/install_cuda.sh
@@ -14,7 +14,7 @@ sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
 sudo apt-get update
 sudo apt-get -y install cuda
 
-python -c "import torch; print('CUDA available: {} ({})'.format(torch.cuda.is_available(), torch.cuda.get_device_name(torch.cuda.current_device())))"
+python3 -c "import torch; print('CUDA available: {} ({})'.format(torch.cuda.is_available(), torch.cuda.get_device_name(torch.cuda.current_device())))"
 
 # clean up
 rm cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
